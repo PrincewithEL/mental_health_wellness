c32f42a9cffaa08125c6418d28dc49087f98697a
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2024-11-07 22:09:25 +0300

    seventeenth commit

--- mental_wellness_Proj/settings.py
+++ mental_wellness_Proj/settings.py
@@ -121,15 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'mental_wellness_App/static',  
-# ]
+# # STATICFILES_DIRS = [
+# #     BASE_DIR / 'mental_wellness_App/static',  
+# # ]
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'mental_wellness_App/static')]
+# STATIC_DIRS = [os.path.join(BASE_DIR, 'mental_wellness_App/static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
